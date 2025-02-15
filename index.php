37c68ec310ed424f32f766135d9c4b1aaa937f25
Lab05_etec <Lab05_etec@etec>
2025-02-14 22:29:23 -0300

    Adiciondo Bootstrap

--- index.php
+++ index.php
@@ -5,11 +5,94 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projeto PWII</title>
+    <link href="bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <a href="contato.php">Página de contato</a>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+        </div>
+    </nav>
+    <a href="contato.php">Página de contato</a><br>
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Abrir modal
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Salvar alterações</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                Depois de salvo não sera possivel mudar!!!
+                            </div>
+                            <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Não salvar</button>
+                    <button type="button" class="btn btn-success">Salvar</button>
+                </div>
+            </div>
+        </div>
+    </div>
     <?php
         echo "<h1>Titulo do projeto PWII B</h1>";
     ?>
+    <div class="dropdown">
+        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown links de acesso
+        </a>
+
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Site 1</a></li>
+            <li><a class="dropdown-item" href="#">Site 2</a></li>
+            <li><a class="dropdown-item" href="#">Site 3</a></li>
+        </ul>
+    </div>
+    <div class="alert alert-success" role="alert">
+    A simple success alert—check it out!
+    </div>
+    <button type="button" class="btn btn-primary">Primary</button>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">Natã</li>
+        <li class="list-group-item">Leonardo</li>
+        <li class="list-group-item">Luis</li>
+        <li class="list-group-item">João</li>
+        <li class="list-group-item">André</li>
+    </ul>
+    <script src="bootstrap.bundle.min.js"></script>
 </body>
 </html>
