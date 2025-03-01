ed9384af39cbe63ed9b212a1a6a14bda3f8d3046
Lab05_etec <Lab05_etec@etec>
2025-02-28 22:28:51 -0300

    Aula 28/02 (If)

--- if.php
+++ if.php
@@ -36,6 +36,108 @@
   </div>
 </nav>
     
+<div class="container">
+  <h1>If</h1>
+  <p>
+    A estrutura de decisão é composta por um teste booleano.
+    Esse teste verifica uma condição se verdadeiro faça uma coisa se falso faça outra.
+    <pre>
+      if($numero == 1){
+        echo "Aqui significa $numero igual a 1";
+      }else{
+        echo "Aqui significa $numero diferente de 1";
+      }
+    </pre>
+
+    <?php
+      $numero = 2;
+      if($numero == 1){
+        echo 'Aqui significa $numero igual a 1';
+      }else{
+        echo 'Aqui significa $numero diferente de 1';
+      }
+    ?>
+  </p>
+
+
+  <h2>Estrutura de decisão encadeada</h2>
+  <p>
+    São varios testes um seguido do outro até algum teste verdadeiro ser encontrado.
+    Caso não encontre nenhum positivo o "else" é executado.
+    Ao encontrar um teste positivo todos os subsequentes não serão executados.
+    <pre>
+      $nome = "Luis"
+      if($nome == "João"){
+        echo "$nome é igual a João";
+      }else if($nome == "Maria"){
+        echo "$nome é igual a Maria";
+      }else if($nome == "Luis"){
+        echo "$nome é igual a Luis";
+      }else if($nome == "Felipe"){
+        echo "$nome é igual a Felipe";
+      }else{
+        echo "Nenhum nome é igual o conteudo da variavel";
+      }
+    </pre>
+      
+    <?php
+      $nome = "Luis";
+      if($nome == "João"){
+        echo '$nome é igual a João';
+      }else if($nome == "Maria"){
+        echo '$nome é igual a Maria';
+      }else if($nome == "Luis"){
+        echo '$nome é igual a Luis';
+      }else if($nome == "Felipe"){
+        echo '$nome é igual a Felipe';
+      }else{
+        echo "Nenhum nome é igual o conteudo da variavel";
+      }
+    ?>
+  </p>
+
+
+  <h2>Varios testes em um If</h2>
+  <p>
+    Em uma estrutura de decisão unica eu consigo realizar dois ou mais testes.
+    Neste caso eu preciso utilizar os conectivos "AND" ou "OR" (E ou OU).
+    <h4>Conectivo "AND" (E)</h4>
+    No exemplo abaixo eu necessito que todas as condições resultem em verdadeiro.
+    <pre>
+      $numero2 = 35;
+      if($numero2 > 10 && $numero2 < 50){
+        echo "$numero2 está entre 10 e 50";
+      }
+      if($numero2 > 36 && $numero2 < 50){
+        //este codigo não será executado
+      }else{
+        echo "$numero2 não está entre 36 e 50";
+      }
+    </pre>
+    <?php
+      $numero2 = 35;
+      if($numero2 > 10 && $numero2 < 50){
+        echo '$numero2 está entre 10 e 50<br>';
+      }
+      if($numero2 > 36 && $numero2 < 50){
+        //este codigo não será executado
+      }else{
+        echo '$numero2 não está entre 36 e 50';
+      }
+    ?>
+  </p>
+
+  <h4>Conectivo "OR" (OU)</h4>
+  <p>
+    O conectivo "OU" testa todas as condições e executa o código se apenas uma
+    das condições for verdadeira. Caso nenhuma condição for verdadeira o else é executado.
+    <pre>
+      
+    </pre>
+  </p>
+</div>
+
+
 <script src="bootstrap.bundle.min.js"></script>
 </body>
 </html>
