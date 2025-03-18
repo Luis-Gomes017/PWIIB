431105cecd5658b78635bc0f997acf38ad13878c
Lab05_etec <Lab05_etec@etec>
2025-03-18 19:20:40 -0300

    Bingo

--- bingo.php
+++ bingo.php
@@ -11,97 +11,101 @@
     <div id="Vertical">
         Vertical
     </div>
+
     <div id="Numeros">
-        <div class="Letras">
+        <div class="letras">
             <div>B</div>
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
-            <div>5</div>
-            <div>6</div>
-            <div>7</div>
-            <div>8</div>
-            <div>9</div>
-            <div>10</div>
-            <div>11</div>
-            <div>12</div>
-            <div>13</div>
-            <div>14</div>
-            <div>15</div>
+            <div class="clique">1</div>
+            <div class="clique">2</div>
+            <div class="clique">3</div>
+            <div class="clique">4</div>
+            <div class="clique">5</div>
+            <div class="clique">6</div>
+            <div class="clique">7</div>
+            <div class="clique">8</div>
+            <div class="clique">9</div>
+            <div class="clique">10</div>
+            <div class="clique">11</div>
+            <div class="clique">12</div>
+            <div class="clique">13</div>
+            <div class="clique">14</div>
+            <div class="clique">15</div>
         </div>
-        <div class="Letras">
+        <div class="letras">
             <div>I</div>
-            <div>16</div>
-            <div>17</div>
-            <div>18</div>
-            <div>19</div>
-            <div>20</div>
-            <div>21</div>
-            <div>22</div>
-            <div>23</div>
-            <div>24</div>
-            <div>25</div>
-            <div>26</div>
-            <div>27</div>
-            <div>28</div>
-            <div>29</div>
-            <div>30</div>
+            <div class="clique">16</div>
+            <div class="clique">17</div>
+            <div class="clique">18</div>
+            <div class="clique">19</div>
+            <div class="clique">20</div>
+            <div class="clique">21</div>
+            <div class="clique">22</div>
+            <div class="clique">23</div>
+            <div class="clique">24</div>
+            <div class="clique">25</div>
+            <div class="clique">26</div>
+            <div class="clique">27</div>
+            <div class="clique">28</div>
+            <div class="clique">29</div>
+            <div class="clique">30</div>
         </div>
-        <div class="Letras">
+        <div class="letras">
             <div>N</div>
-            <div>31</div>
-            <div>32</div>
-            <div>33</div>
-            <div>34</div>
-            <div>35</div>
-            <div>36</div>
-            <div>37</div>
-            <div>38</div>
-            <div>39</div>
-            <div>40</div>
-            <div>41</div>
-            <div>42</div>
-            <div>43</div>
-            <div>44</div>
-            <div>45</div>
+            <div class="clique">31</div>
+            <div class="clique">32</div>
+            <div class="clique">33</div>
+            <div class="clique">34</div>
+            <div class="clique">35</div>
+            <div class="clique">36</div>
+            <div class="clique">37</div>
+            <div class="clique">38</div>
+            <div class="clique">39</div>
+            <div class="clique">40</div>
+            <div class="clique">41</div>
+            <div class="clique">42</div>
+            <div class="clique">43</div>
+            <div class="clique">44</div>
+            <div class="clique">45</div>
         </div>
-        <div class="Letras">
+        <div class="letras">
             <div>G</div>
-            <div>46</div>
-            <div>47</div>
-            <div>48</div>
-            <div>49</div>
-            <div>50</div>
-            <div>51</div>
-            <div>52</div>
-            <div>53</div>
-            <div>54</div>
-            <div>55</div>
-            <div>56</div>
-            <div>57</div>
-            <div>58</div>
-            <div>59</div>
-            <div>60</div>
+            <div class="clique">46</div>
+            <div class="clique">47</div>
+            <div class="clique">18</div>
+            <div class="clique">49</div>
+            <div class="clique">50</div>
+            <div class="clique">51</div>
+            <div class="clique">52</div>
+            <div class="clique">53</div>
+            <div class="clique">54</div>
+            <div class="clique">55</div>
+            <div class="clique">56</div>
+            <div class="clique">57</div>
+            <div class="clique">58</div>
+            <div class="clique">59</div>
+            <div class="clique">60</div>
         </div>
-        <div class="Letras">
+        <div class="letras">
             <div>O</div>
-            <div>61</div>
-            <div>62</div>
-            <div>63</div>
-            <div>64</div>
-            <div>65</div>
-            <div>66</div>
-            <div>67</div>
-            <div>68</div>
-            <div>69</div>
-            <div>70</div>
-            <div>71</div>
-            <div>72</div>
-            <div>73</div>
-            <div>74</div>
-            <div>75</div>
+            <div class="clique">61</div>
+            <div class="clique">62</div>
+            <div class="clique">63</div>
+            <div class="clique">64</div>
+            <div class="clique">65</div>
+            <div class="clique">66</div>
+            <div class="clique">67</div>
+            <div class="clique">68</div>
+            <div class="clique">69</div>
+            <div class="clique">70</div>
+            <div class="clique">71</div>
+            <div class="clique">72</div>
+            <div class="clique">73</div>
+            <div class="clique">74</div>
+            <div class="clique">75</div>
         </div>
     </div>
+
+    <script src="script.js"></script>
+    
 </body>
 </html>
