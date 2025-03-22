dc1042a63fa587b7910df413931b7864c986042e
Lab05_etec <Lab05_etec@etec>
2025-03-21 22:25:51 -0300

    Exercicios 21/03

--- formulario.php
+++ formulario.php
@@ -37,7 +37,11 @@
   </div>
 </nav>
 
+<div class="container">
 <h1>Cadastro de Usuário</h1>
+<br>
+
+<p>1- Crie um formulário HTML que solicite o nome e a idade de um usuário. Ao enviar o formulário com o método GET, exiba o nome e a idade do usuário na página seguinte.</p>
 <form action="salvar_dados.php" method="get">
     <label>Nome</label>
     <input type="text" name="nome">
@@ -46,10 +50,13 @@
     <input type="number" name="idade">
     <br>
     <button type="submit">
-        Logar
+      Enviar
     </button>
 </form>
+
 <br><br>
+
+<p>2-Crie um formulário HTML que receba dois números e, ao enviar com o método GET, exiba a soma desses dois números.</p>
 <form action="salvar_dados.php" method="get">
     <label>Numero 1</label>
     <input type="number" name="numero1">
@@ -58,19 +65,67 @@
     <input type="number" name="numero2">
     <br>
     <button type="submit">
-        Logar
+        Enviar
     </button>
 </form>
+
 <br><br>
+
+<p>3-Crie um formulário HTML que peça o nome do usuário. Quando o formulário for enviado com o método GET, exiba uma mensagem personalizada dizendo "Olá, [nome]".</p>
 <form action="salvar_dados.php" method="get">
     <label>Nome</label>
     <input type="text" name="nome2">
     <br>
     <button type="submit">
-        Logar
+      Enviar
     </button>
 </form>
+
+<br><br>
+
+<p>4-Crie um formulário de login que receba o nome de usuário e senha. Quando o formulário for enviado com o método POST, verifique se o nome de usuário e a senha são válidos (pode ser um simples exemplo de validação).</p>
+<form action="salvar_dados.php" method="post">
+    <label>Nome (Luis)</label>
+    <input type="text" name="nome3">
+    <br>
+    <label>Senha (4586)</label>
+    <input type="password" name="senha">
+    <br>
+    <button type="submit">
+      Enviar
+    </button>
 </form>
+
+<br><br>
+
+<p>5-Crie um formulário HTML que receba dois números e, ao enviar com o método POST, exiba o produto desses números.</p>
+<form action="salvar_dados.php" method="post">
+    <label>Numero 1</label>
+    <input type="number" name="numero3">
+    <br>
+    <label>Numero 2</label>
+    <input type="number" name="numero4">
+    <br>
+    <button type="submit">
+      Enviar
+    </button>
+</form>
+
+<br><br>
+
+<p>6-Crie um formulário HTML que receba o nome e a mensagem de um usuário. Quando o formulário for enviado com o método POST, exiba a mensagem do usuário.</p>
+<form action="salvar_dados.php" method="post">
+    <label>Nome</label>
+    <input type="text" name="nome4">
+    <br>
+    <label>Mensagem</label>
+    <input type="text" name="mensagem">
+    <br>
+    <button type="submit">
+      Enviar
+    </button>
+</form>
+</div>
 
 <script src="bootstrap.bundle.min.js"></script>
 </body>
--- salvar_dados.php
+++ salvar_dados.php
@@ -2,17 +2,39 @@
     echo $_GET["nome"];
     echo "<br>";
     echo $_GET["idade"];
+
     echo "<br><br>";
+
     $numero1 = $_GET["numero1"];
     $numero2 = $_GET["numero2"];
     echo $numero1 + $numero2;
+
     echo "<br><br>";
-    $nome = $_GET["nome2"];
-    echo "Olá, " , $nome;
 
+    $nome2 = $_GET["nome2"];
+    echo "Olá, " , $nome2;
 
+    echo "<br><br>";
 
+    $nome3 = $_POST["nome3"];
+    $senha = $_POST["senha"];
+    if($nome3 == "Luis" && $senha == "4586"){
+        echo "Usuario e senha correto";
+    }else{
+        echo "Usuario e senha incorreto";
+    }
 
+    echo "<br><br>";
+
+    $numero3 = $_POST["numero3"];
+    $numero4 = $_POST["numero4"];
+    echo $numero3 * $numero4;
+
+    echo "<br><br>";
+
+    $nome4 = $_POST["nome4"];
+    $mensagem = $_POST["mensagem"];
+    echo "A mensagem do " , $nome4 , " é: ", $mensagem;
 
 
 ?>
